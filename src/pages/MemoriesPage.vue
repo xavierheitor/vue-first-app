7ce97560a316a22bb7660df1c3e80eb40307739e
Heitor Xavier <xavierheitor@hotmail.com>
2022-02-16 17:16:13 -0300

    inicial

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>All memories</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <h2>Theese are my memories</h2>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonTitle, IonContent, IonToolbar } from '@ionic/vue';
+
+export default {
+    components: {
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonContent,
+        IonToolbar
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+
+import MemoriesPage from '../pages/MemoriesPage.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/memories',
+    component: MemoriesPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
